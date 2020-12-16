74548c6607987f4e69e14e88e47f1419ceedfa29
Ndn1618 <ndilbar1618@gmail.com>
2020-12-16 20:58:15 +0500

    Add dark and light themes object

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import { useState } from 'react'
+import {darkTheme, lightTheme} from './Components/Themes'
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState('light')
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
   return (
     <>
-      <div className='theme theme--dark'>
+      <div className={theme}>
         <h1 className='sr-only'>Login Form with React.js</h1>
-
+        <div className={themeMode.togglerText}>
+          <label class="switch mr-3">
+            <input onChange={ () => setTheme('dark')} type="checkbox" />
+            <span class="slider round"></span>
+          </label>
+          Switch dark theme
+        </div>
         <section className='py-5 login-from-section'>
           <div className="login-form-wrapper container p-5 rounded">
-            <h2 className='text-center mb-3 text-light'>User Login</h2>
-            <form className='login-form d-flex flex-column justify-content-center p-4 rounded' action='https://echo.htmlacademy.ru' method='POST'>
-              <label className='label h5 rounded' htmlFor='username-input'>Username</label>
+            <h2 className={themeMode.loginHeading}>User Login</h2>
+            <form className={themeMode.loginForm} action='https://echo.htmlacademy.ru' method='POST'>
+              <label className={themeMode.loginLabel} htmlFor='username-input'>Username</label>
               <input className='form-control mb-4' id='username-input' type='text' name='username' placeholder='Username' autoComplete='off' />
-              <label className='label h5 rounded' htmlFor='password-input'>Password</label>
+              <label className={themeMode.loginLabel} htmlFor='password-input'>Password</label>
               <input className='form-control mb-4' id='password-input' type='password' name='password' placeholder='Password' />
               <button className='btn btn-light border align-self-center px-4 font-weight-bold' type='submit'>Login</button>
             </form>
