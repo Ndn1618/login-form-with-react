8df800119e65f38d8ee13e25ca2b7f4b9ef948ec
Ndn1618 <ndilbar1618@gmail.com>
2020-12-16 18:26:18 +0500

    Add styles of dark and light themes

--- src/App1.js
+++ src/App1.js
@@ -3,13 +3,13 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className='theme theme--dark'>
+      <div className='theme theme--light'>
         <h1 className='sr-only'>Login Form with React.js</h1>
 
         <section className='py-5 login-from-section'>
           <div className="login-form-wrapper container p-5 rounded">
-            <h2 className='text-center mb-3 text-light'>User Login</h2>
-            <form className='d-flex flex-column justify-content-center bg-light p-4 rounded' action='https://echo.htmlacademy.ru' method='POST'>
+            <h2 className='text-center mb-3'>User Login</h2>
+            <form className='d-flex flex-column justify-content-center bg-light shadow-lg p-4 rounded' action='https://echo.htmlacademy.ru' method='POST'>
               <label className='h5' htmlFor='username-input'>Username</label>
               <input className='form-control mb-4' id='username-input' type='text' name='username' placeholder='Username' autoComplete='off' />
               <label className='h5' htmlFor='password-input'>Password</label>
